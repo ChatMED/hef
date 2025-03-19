8705b6e7bd9e7206e747070ee55a13ea59c6ae12
atodorovska <ana5todorovska@gmail.com>
2025-03-19 22:05:40 +0100

    feat: add option to test on dummy questions

--- src/main/java/io/chatmed/evaluation_platform/config/DataInitializer.java
+++ src/main/java/io/chatmed/evaluation_platform/config/DataInitializer.java
@@ -25,8 +25,12 @@ import java.util.Objects;
 @Component
 public class DataInitializer {
 
-    //    private static final String DATA_LOCATION = "classpath:data/*.json";
-    private static final String DATA_LOCATION = "classpath:test/*.json";
+    /*
+            Work with test folder for testing, data for production.
+     */
+
+    private static final String DATA_LOCATION = "classpath:data/*.json";
+//    private static final String DATA_LOCATION = "classpath:test/*.json";
 
     private final ModelService modelService;
     private final QuestionService questionService;
@@ -41,11 +45,8 @@ public class DataInitializer {
             VersionService versionService,
             ObjectMapper objectMapper
     ) {
-        this.modelService = modelService;
-        this.questionService = questionService;
-        this.answerService = answerService;
-        this.versionService = versionService;
-        this.objectMapper = objectMapper;
+        this.modelService = modelService; this.questionService = questionService; this.answerService = answerService;
+        this.versionService = versionService; this.objectMapper = objectMapper;
     }
 
     private void initModels() throws IOException {
@@ -55,12 +56,10 @@ public class DataInitializer {
         Resource[] resources = resolver.getResources(DATA_LOCATION);
 
         for (Resource resource : resources) {
-            String fileName = Objects.requireNonNull(resource.getFilename());
-            if (fileName.endsWith(".json")) {
+            String fileName = Objects.requireNonNull(resource.getFilename()); if (fileName.endsWith(".json")) {
                 modelNames.add(fileName.replace(".json", ""));
             }
-        }
-        modelService.createNewModels(modelNames);
+        } modelService.createNewModels(modelNames);
     }
 
     private Version initVersion() {
@@ -70,8 +69,7 @@ public class DataInitializer {
 
     @PostConstruct
     public void initData() throws IOException {
-        initModels();
-        Version version = initVersion();
+        initModels(); Version version = initVersion();
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resolver.getResources(DATA_LOCATION);
@@ -83,25 +81,18 @@ public class DataInitializer {
 
                 modelService.findByName(modelName).ifPresent(model -> {
                     rootNode.fields().forEachRemaining(entry -> {
-                        Long key = Long.parseLong(entry.getKey());
-                        JsonNode node = entry.getValue();
+                        Long key = Long.parseLong(entry.getKey()); JsonNode node = entry.getValue();
 
                         Question question = questionService.findByQuestionKey(key).orElseGet(Question::new);
                         if (question.getId() == null) {
-                            question.setQuestionKey(key);
-                            question.setText(node.get("question").asText());
-                            question.setVersion(version);
-                            questionService.save(question);
+                            question.setQuestionKey(key); question.setText(node.get("question").asText());
+                            question.setVersion(version); questionService.save(question);
                         }
 
-                        Answer answer = answerService.findByQuestionAndModel(question, model)
-                                                     .orElseGet(Answer::new);
+                        Answer answer = answerService.findByQuestionAndModel(question, model).orElseGet(Answer::new);
                         if (answer.getId() == null) {
-                            answer.setAnswerKey(key);
-                            answer.setText(node.get("answer").asText());
-                            answer.setQuestion(question);
-                            answer.setModel(model);
-                            answerService.save(answer);
+                            answer.setAnswerKey(key); answer.setText(node.get("answer").asText());
+                            answer.setQuestion(question); answer.setModel(model); answerService.save(answer);
                         }
                     });
                 });
