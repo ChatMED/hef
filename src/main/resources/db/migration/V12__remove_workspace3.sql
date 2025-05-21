72e53641676a703beee9a21341d2d2fc3edb5b90
atodorovska <ana5todorovska@gmail.com>
2025-05-21 15:05:29 +0200

    feat: add workspace 3 with correct gpt model

---
+++ src/main/resources/db/migration/V12__remove_workspace3.sql
@@ -0,0 +1,9 @@
+delete from evaluations where evaluations.workspace_id = 3;
+
+delete from answers where answers.workspace_id = 3;
+
+delete from models where models.workspace_id = 3;
+
+delete from memberships where memberships.workspace_id = 3;
+
+delete from questions where questions.workspace_id = 3;
